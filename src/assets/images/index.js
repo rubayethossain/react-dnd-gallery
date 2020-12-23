842ce933f71d74fcd6c05cc491569aefe9e2c627
Md. Rubayet Hossain <rubayet.dev@gmail.com>
2020-12-23 20:09:29 +0600

    Add image filter icons

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,4 +1,15 @@
 import cogIcon from "./cog.png";
 import trashIcon from "./trash.png";
+import invertIcon from "./invert.png";
+import opacityIcon from "./opacity.png";
+import brightnessIcon from "./brightness.png";
+import contrastIcon from "./contrast.png";
 
-export { cogIcon, trashIcon };
+export {
+  cogIcon,
+  trashIcon,
+  invertIcon,
+  opacityIcon,
+  brightnessIcon,
+  contrastIcon,
+};
