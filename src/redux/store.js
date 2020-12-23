a6fee054ea95893157380f16ba55c25063cf621a
Md. Rubayet Hossain <rubayet.dev@gmail.com>
2020-12-23 23:47:12 +0600

    Save and retrieve redux store from localStorage

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,9 +3,40 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import rootReducer from "./rootReducer";
 
+// Save state to localStorage
+const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem("state", serializedState);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem("state");
+    if (serializedState === null) {
+      return undefined;
+    }
+    return JSON.parse(serializedState);
+  } catch (err) {
+    return undefined;
+  }
+};
+
+const persistedState = loadState();
+
 const store = createStore(
   rootReducer,
+  persistedState,
   composeWithDevTools(applyMiddleware(thunk))
 );
+
+store.subscribe(() => {
+  saveState({
+    media: store.getState().media,
+  });
+});
 
 export default store;
